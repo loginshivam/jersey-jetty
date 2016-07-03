497ea59233defc8d14cbbd8132c242e6c527d8d8
loginshivam@gmail.com <loginshivam@gmail.com>
2016-07-03 17:38:18 +0530

    SSE events

--- src/test/java/com/sample/jersey/Main.java
+++ src/test/java/com/sample/jersey/Main.java
@@ -7,23 +7,20 @@ import java.net.URL;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		while(true){
-			
+		while (true) {
 			try {
 				URL url = new URL("http://localhost:8071/jersey/data");
 				try {
-				System.out.println(url.openConnection().getContent());
+					System.out.println(url.openConnection().getContent());
 					Thread.sleep(3000);
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 	}
-	
+
 }
